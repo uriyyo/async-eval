6ed6371fa8a175df49baefa81376dfde4f939623
Yurii Karabas <1998uriyyo@gmail.com>
2021-08-30 10:01:59 +0300

    🐛 Fix pydevd integration

--- async_eval/ext/pydevd/integration.py
+++ async_eval/ext/pydevd/integration.py
@@ -1,12 +1,22 @@
 import inspect
 
+import nest_asyncio
+
 from async_eval import async_eval, asyncio_patch
 
 from . import code
 
 
 def generate_main_script() -> str:
-    return "\n".join(inspect.getsource(m) for m in (asyncio_patch, async_eval, code))
+    return "\n".join(
+        inspect.getsource(m)
+        for m in (
+            nest_asyncio,
+            asyncio_patch,
+            async_eval,
+            code,
+        )
+    )
 
 
 __all__ = ["generate_main_script"]
